f8e5848a44493ad82599e929ba766c8ba6664002
wosmaker <wosmaker@gmail.com>
2019-05-11 23:11:16 +0700

    redirect part

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 // PHP permanent URL redirection
-header("Location: http://www.domain.com/new-page.php", true, 301);
+header("Location: http://www.sdbr.space/public", true, 301);
 exit();
 ?>
