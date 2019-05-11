b12a5d500101df47dcde7b104f2215cb321df659
wosmaker <wosmaker@gmail.com>
2019-05-11 23:14:29 +0700

    redirect path

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 // PHP permanent URL redirection
-header("Location: http://www.sdbr.space/public", true, 301);
+header("Location:http://sdbrv2.herokuapp.com/public/", true, 301);
 exit();
 ?>
