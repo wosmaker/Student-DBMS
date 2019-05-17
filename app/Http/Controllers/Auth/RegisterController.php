d6522f8217ad189a57b4fbeb1218245edb5fc907
BOB <20367534+ttpjb@users.noreply.github.com>
2019-05-17 17:13:53 +0700

    edit register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,9 +50,9 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'userroleid' => ['required', 'number'],
+            'userroleid' => ['required', 'integer'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed']
+            'password' => ['required', 'string', 'min:2', 'confirmed']
         ]);
     }
 
